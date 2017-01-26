f121131f0ebd4411d06e31cc37799e8e91ff8ea5
opaluch.lukasz <opaluch.lukasz@gmail.com>
2017-01-26 21:17:05 +0100

    - multiple imports for same feature support

--- src/main/java/com/github/opaluchlukasz/junit2spock/core/Supported.java
+++ src/main/java/com/github/opaluchlukasz/junit2spock/core/Supported.java
@@ -1,30 +1,33 @@
 package com.github.opaluchlukasz.junit2spock.core;
 
-import org.junit.Test;
-
 import java.util.List;
 
+import static com.google.common.collect.ImmutableList.of;
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
 
 public enum Supported {
-    TEST_ANNOTATION(Test.class.getName(), false),
-    ASSERT_EQUALS("org.junit.Assert.assertEquals", true),
-    ASSERT_NOT_NULL("org.junit.Assert.assertNotNull", true);
+    TEST_ANNOTATION(of("org.junit.Test"), false),
+    ASSERT_EQUALS(of("org.junit.Assert.assertEquals", "junit.framework.Assert.assertEquals"), true),
+    ASSERT_NOT_NULL(of("org.junit.Assert.assertNotNull", "junit.framework.Assert.assertEquals"), true);
 
-    private String imported;
+    private List<String> imports;
     private boolean testMethodFeature;
 
-    Supported(String imported, boolean testMethodFeature) {
-        this.imported = imported;
+    Supported(List<String> imports, boolean testMethodFeature) {
+        this.imports = imports;
         this.testMethodFeature = testMethodFeature;
     }
 
     public static List<String> imports() {
-        return stream(values()).map(supported -> supported.imported).collect(toList());
+        return stream(values())
+                .flatMap(supported -> supported.imports.stream())
+                .collect(toList());
     }
 
     public static List<Supported> testMethodFeatures() {
-        return stream(values()).filter(supported -> supported.testMethodFeature).collect(toList());
+        return stream(values())
+                .filter(supported -> supported.testMethodFeature)
+                .collect(toList());
     }
 }
